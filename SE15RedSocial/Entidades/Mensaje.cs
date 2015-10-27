d6408efbc96d9f5fa9a195def2d0de09c33d0abd
Yuuki16 <yuuki.monge16@gmail.com>
2015-10-26 20:46:15 -0600

    Mensaje 1.0

---
+++ SE15RedSocial/Entidades/Mensaje.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Mensaje
+    {
+        #region Atributos
+
+        private int id;
+        private int emisor;
+        private int receptor;
+        private string estampa;
+        private string mensaje;
+        private string multimedia;
+
+        #endregion
+
+        #region Propiedades
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.id = value;
+                }
+            }
+        }
+
+        public int Emisor
+        {
+            get
+            {
+                return this.emisor;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.emisor = value;
+                }
+            }
+        }
+
+        public int Receptor
+        {
+            get
+            {
+                return this.receptor;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.receptor = value;
+                }
+            }
+        }
+
+        public string Estampa
+        {
+            get
+            {
+                return this.estampa;
+            }
+            set
+            {
+                if (value != string.Empty )
+                {
+                    this.estampa = value;
+                }
+            }
+        }
+
+        public string Mensaje
+        {
+            get
+            {
+                return this.mensaje;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.mensaje = value;
+                }
+            }
+        }
+
+        public string Multimedia
+        {
+            get
+            {
+                return this.multimedia;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.multimedia = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructores
+
+        public Mensaje() { }
+
+        public Mensaje(int p_id, int p_emisor, int p_receptor, string p_estampa, string p_mensaje, string p_multimedia) { }
+        
+        #endregion
+
+        #region Metodos
+        #endregion
+    }
+}
+
