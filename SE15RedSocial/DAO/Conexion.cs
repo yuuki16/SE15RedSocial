865fe73029bbbc4a6947c9d0818b2e42e9dc5178
Yuuki16 <yuuki.monge16@gmail.com>
2015-10-27 16:31:07 -0600

    Conexion 1.0 e Inclusión DAO_USUARIO

---
+++ SE15RedSocial/DAO/Conexion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public class Conexion
+    {
+
+        public SqlConnection conn;
+        Boolean resultado = false;
+
+        public Boolean abrirConexion()
+        {
+
+            try
+            {
+                conn.ConnectionString = DAO.Properties.Settings.Default.connectionString;
+                conn.Open();
+                resultado = true;
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public Boolean cerrarConexion()
+        {
+
+            try
+            {
+                if (conn != null)
+	            {
+		            conn.Close();
+                    resultado = true;
+                    return resultado;
+	            }
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+    }
+}
---
+++ SE15RedSocial/DAO/DAO_Usuario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAO
+{
+    public class DAO_Usuario
+    {
+        Conexion conn = new Conexion();
+        Boolean resultado;
+        string sql;
+        public Boolean ObtenerUsuario()
+        {
+            sql = "SP_ObtenerUsuario";
+            if (conn.abrirConexion() == true) { 
+                
+            }
+            return resultado;
+        }
+        
+        public Boolean AgregarUsuario() {
+           
+            sql = "SP_AgregarUsuario";
+
+            try
+            {
+                if (conn.abrirConexion() == true) {
+                    SqlCommand comando = new SqlCommand(sql, conn.conn);
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.ExecuteNonQuery();
+                    resultado = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+
+            return resultado;
+        }
+
+        public Boolean ModificarUsuario()
+        {
+
+            sql = "SP_ModificarUsuario";
+
+            return resultado;
+        }
+
+        public Boolean LoginUsuario()
+        {
+
+            sql = "SP_LoginUsuario";
+            Boolean resultado = true;
+
+
+
+            return resultado;
+        }
+
+
+    }
+}
