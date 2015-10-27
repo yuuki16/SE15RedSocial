d8ef4642c030166a98960e9df3b5ef7bbaff86dc
Yuuki16 <yuuki.monge16@gmail.com>
2015-10-27 12:16:20 -0600

    Publicacion 1.0

---
+++ SE15RedSocial/Entidades/Publicacion.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Publicacion
+    {
+        #region Atributos
+
+        private int id;
+        private int usuario;
+        private string publicacion;
+        private string estampa;
+        private char estado;
+        #endregion
+
+        #region Propiedades
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.id = value;
+                }
+            }
+        }
+
+        public int Usuario
+        {
+            get
+            {
+                return this.usuario;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.usuario = value;
+                }
+            }
+        }
+
+        public string Publicacion
+        {
+            get
+            {
+                return this.publicacion;
+            }
+            set
+            {
+                if (value != string.Empty )
+                {
+                    this.publicacion = value;
+                }
+            }
+        }
+
+        public string Estampa
+        {
+            get
+            {
+                return this.estampa;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.estampa = value;
+                }
+            }
+        }
+
+        public char Estado
+        {
+            get
+            {
+                return this.estado;
+            }
+            set
+            {
+                this.estado = value;
+            }
+        }
+        #endregion
+
+        #region Constructores
+        public Publicacion() { }
+
+        public Publicacion(int p_id, int p_usuario, string p_publicacion, string p_estampa, char p_estado) {
+            this.id = p_id;
+            this.usuario = p_usuario;
+            this.publicacion = p_publicacion;
+            this.estampa = p_estampa;
+            this.estado = p_estado;
+        }
+        #endregion
+
+        #region Metodos
+        #endregion
+    }
+}
